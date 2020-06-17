0dfb634d92d34f3f249256d860b3f99aeb609b98
Timo Weiser <timo.weiser@gmail.com>
2020-06-17 09:57:47 +0200

    Update Plugin.swift

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -6,14 +6,12 @@ import AuthenticationServices
 typealias JSObject = [String:Any]
 @objc(SingleSignOn)
 public class SingleSignOn: CAPPlugin, ASWebAuthenticationPresentationContextProviding {
-
     @available(iOS 12.0, *)
     public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
-        return DispatchQueue.main.sync {
-           return UIApplication.shared.keyWindow!
-        }
+        return UIApplication.shared.windows.first!;
     }
     
+
     private var session: Any?
 
     @objc func authenticate(_ call: CAPPluginCall) {
@@ -23,7 +21,7 @@ public class SingleSignOn: CAPPlugin, ASWebAuthenticationPresentationContextProv
         if #available(iOS 12.0, *) {
             self.session = ASWebAuthenticationSession.init(url: URL(string: url)!, callbackURLScheme: scheme, completionHandler: { url, error in
                 if (error != nil) {
-                    call.error("Error", error)
+                    call.reject("")
                 }
                 else {
                     var response = JSObject()
@@ -33,13 +31,15 @@ public class SingleSignOn: CAPPlugin, ASWebAuthenticationPresentationContextProv
             })
             if #available(iOS 13.0, *) {
                 (self.session as! ASWebAuthenticationSession).presentationContextProvider = self
-            }
+            } else {
+                // Fallback on earlier versions
+            };
             (self.session as! ASWebAuthenticationSession).start()
         }
         else if #available(iOS 11.0, *) {
             self.session = SFAuthenticationSession.init(url: URL(string: url)!, callbackURLScheme: scheme, completionHandler: { url, error in
                 if (error != nil) {
-                    call.error("Error", error)
+                    call.reject("")
                 }
                 else {
                     var response = JSObject()
@@ -50,7 +50,7 @@ public class SingleSignOn: CAPPlugin, ASWebAuthenticationPresentationContextProv
             (self.session as! SFAuthenticationSession).start()
         }
         else {
-            call.error("Not supported")
+            call.reject("Not supported")
         }
     }
 
