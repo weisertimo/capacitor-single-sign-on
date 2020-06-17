7ee96377e3490b147472223f232d9a825a350d20
Timo Weiser <timo.weiser@codify.ch>
2020-06-17 10:44:22 +0200

    added build

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    interface PluginRegistry {
+        SingleSignOn?: SingleSignOn;
+    }
+}
+export interface SingleSignOn {
+    authenticate(options: {
+        url: string;
+        customScheme?: string;
+    }): Promise<{
+        url: string;
+    }>;
+}
